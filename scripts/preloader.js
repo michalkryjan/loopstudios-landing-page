66c2d72f40a5c370ce8f1a16a16c69e050810762
michalkryjan <michal.kryjan@gmail.com>
2022-06-03 22:55:49 +0200

    preloader added

---
+++ scripts/preloader.js
@@ -0,0 +1,18 @@
+document.onreadystatechange = setTimeout(() => {
+    let preloader = document.querySelector("#preloader");
+    let preloader__logo = document.querySelector(".preloader__logo");
+    let preloader__svg = document.querySelector(".preloader__svg");
+
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        preloader.style.visibility = "visible";
+    } else {
+        preloader.style.opacity = "0";
+        preloader__logo.style.opacity = "0";
+        preloader__svg.style.opacity = "0";
+        document.querySelector("body").style.visibility = "visible";
+        setTimeout(function() {
+            preloader.remove();
+        }, 2500);
+    }
+}, 1200)
